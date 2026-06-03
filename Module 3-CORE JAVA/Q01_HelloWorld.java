ccba4646cbf9a6225bc1d98597070e6d5c090f17
Rishav Kumar <rishavkumar14012004@gmail.com>
2026-06-03 21:28:04 +0530

    add files

--- Module 3-CORE JAVA/Q01_HelloWorld.java
+++ Module 3-CORE JAVA/Q01_HelloWorld.java
@@ -1,1 +1,5 @@
-public class Q01_HelloWorld{public static void main(String[]a){System.out.println("Hello, World!");}}
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
