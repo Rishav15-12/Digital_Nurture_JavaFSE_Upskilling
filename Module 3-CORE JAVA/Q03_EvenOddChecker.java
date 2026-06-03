a94fb0e289044e089ca65f1131c6ffb50437d01a
Rishav Kumar <rishavkumar14012004@gmail.com>
2026-06-03 21:26:19 +0530

    add files

--- Module 3-CORE JAVA/Q03_EvenOddChecker.java
+++ Module 3-CORE JAVA/Q03_EvenOddChecker.java
@@ -1,1 +1,18 @@
-import java.util.*; public class Q03_EvenOddChecker{public static void main(String[]a){int n=new Scanner(System.in).nextInt();System.out.println(n%2==0?"Even":"Odd");}}
+import java.util.Scanner;
+
+public class Q03_EvenOddChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int number = sc.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even.");
+        } else {
+            System.out.println(number + " is Odd.");
+        }
+
+        sc.close();
+    }
+}
