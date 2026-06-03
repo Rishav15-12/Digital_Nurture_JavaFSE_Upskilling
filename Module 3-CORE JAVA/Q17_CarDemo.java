700429cba5870873c42dba336d0405a076125e62
Rishav Kumar <rishavkumar14012004@gmail.com>
2026-06-03 21:39:41 +0530

    add files

--- Module 3-CORE JAVA/Q17_CarDemo.java
+++ Module 3-CORE JAVA/Q17_CarDemo.java
@@ -1,1 +1,23 @@
-public class Q17_CarDemo{public static void main(String[]a){Car c=new Car();c.make="Toyota";c.model="Camry";c.year=2024;c.displayDetails();}}
+class Car {
+    String make;
+    String model;
+    int year;
+
+    void displayDetails() {
+        System.out.println("Make: " + make);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+}
+
+public class Q17_CarDemo {
+    public static void main(String[] args) {
+
+        Car c = new Car();
+        c.make = "Toyota";
+        c.model = "Camry";
+        c.year = 2024;
+
+        c.displayDetails();
+    }
+}
