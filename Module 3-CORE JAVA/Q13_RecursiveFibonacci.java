08820eb96584e69565a06b039093ef4a53ad67af
Rishav Kumar <rishavkumar14012004@gmail.com>
2026-06-03 21:14:17 +0530

    add files

--- Module 3-CORE JAVA/Q13_RecursiveFibonacci.java
+++ Module 3-CORE JAVA/Q13_RecursiveFibonacci.java
@@ -1,1 +1,23 @@
-public class Q13_RecursiveFibonacci{static int f(int n){return n<=1?n:f(n-1)+f(n-2);} public static void main(String[]a){System.out.println(f(10));}}
+import java.util.Scanner;
+
+public class Q13_RecursiveFibonacci {
+
+    static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a positive integer: ");
+        int n = sc.nextInt();
+
+        System.out.println("Fibonacci number at position " + n +
+                           " = " + fibonacci(n));
+
+        sc.close();
+    }
+}
