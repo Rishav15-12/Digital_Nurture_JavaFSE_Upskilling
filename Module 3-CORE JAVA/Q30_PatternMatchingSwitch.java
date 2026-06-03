4b10d7e2552bafa58b2ec13b52cc3a21857e2799
Rishav Kumar <rishavkumar14012004@gmail.com>
2026-06-03 20:54:38 +0530

    add files

--- Module 3-CORE JAVA/Q30_PatternMatchingSwitch.java
+++ Module 3-CORE JAVA/Q30_PatternMatchingSwitch.java
@@ -1,1 +1,33 @@
-public class Q30_PatternMatchingSwitch{static void test(Object o){switch(o){case Integer i->System.out.println(i);case String s->System.out.println(s);default->System.out.println("Other");}} public static void main(String[]a){test(5);}}
+public class Q30_PatternMatchingSwitch {
+
+    static void test(Object obj) {
+        switch (obj) {
+            case Integer i ->
+                System.out.println("Integer value: " + i);
+
+            case String s ->
+                System.out.println("String value: " + s);
+
+            case Double d ->
+                System.out.println("Double value: " + d);
+
+            case Long l ->
+                System.out.println("Long value: " + l);
+
+            case null ->
+                System.out.println("Object is null");
+
+            default ->
+                System.out.println("Unknown type: " + obj);
+        }
+    }
+
+    public static void main(String[] args) {
+        test(100);
+        test("Hello Java 21");
+        test(45.67);
+        test(999L);
+        test(true);
+        test(null);
+    }
+}
