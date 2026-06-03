9833986a593934b46db560c6fff05956ff121f24
Rishav Kumar <rishavkumar14012004@gmail.com>
2026-06-03 21:19:26 +0530

    add files

--- Module 3-CORE JAVA/Q08_OperatorPrecedence.java
+++ Module 3-CORE JAVA/Q08_OperatorPrecedence.java
@@ -1,1 +1,13 @@
-public class Q08_OperatorPrecedence{public static void main(String[]a){System.out.println(10+5*2);}}
+public class Q08_OperatorPrecedence {
+    public static void main(String[] args) {
+        int result = 10 + 5 * 2;
+
+        System.out.println("Expression: 10 + 5 * 2");
+        System.out.println("Result: " + result);
+
+        System.out.println("\nExplanation:");
+        System.out.println("Multiplication (*) is performed before addition (+).");
+        System.out.println("5 * 2 = 10");
+        System.out.println("10 + 10 = 20");
+    }
+}
