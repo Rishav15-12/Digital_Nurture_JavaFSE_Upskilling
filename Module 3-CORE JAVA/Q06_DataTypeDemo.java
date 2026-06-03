0dd928becdee72b413f3521b6b98cb988cd3074e
Rishav Kumar <rishavkumar14012004@gmail.com>
2026-06-03 21:23:31 +0530

    add files

--- Module 3-CORE JAVA/Q06_DataTypeDemo.java
+++ Module 3-CORE JAVA/Q06_DataTypeDemo.java
@@ -1,1 +1,15 @@
-public class Q06_DataTypeDemo{public static void main(String[]a){int i=10;float f=2.5f;double d=3.14;char c='A';boolean b=true;System.out.println(i+" "+f+" "+d+" "+c+" "+b);}}
+public class Q06_DataTypeDemo {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 2.5f;
+        double d = 3.14;
+        char c = 'A';
+        boolean b = true;
+
+        System.out.println("Integer Value: " + i);
+        System.out.println("Float Value: " + f);
+        System.out.println("Double Value: " + d);
+        System.out.println("Character Value: " + c);
+        System.out.println("Boolean Value: " + b);
+    }
+}
